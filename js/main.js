98b7e113359eb62eb4257dfb82e0f344f0017714
zstuder25 <zstuder25@gmail.com>
2017-05-22 00:16:26 -0400

    jquery, data usage, and datatables all included

--- js/main.js
+++ js/main.js
@@ -1,5 +1,41 @@
+var populateRow = function(item){
+    var provider = $('<tr/>')
+            .prepend($('<input/>')
+                .attr('type', 'checkbox')
+            )
+            .append($('<td/>')
+                .attr('id', 'name')
+                .text(item['last_name'] + ', ' + item['first_name'])
+            )
+            .append($('<td/>')
+                .attr('id', 'specialty')
+                .text(item['specialty'])
+            )
+            .append($('<td/>')
+                .text(item['email_address'])
+            )
+            .append($('<td/>')
+                .text(item['practice_name'])
+            );
+
+        $('#tableBody').append(provider);
+}
+
+//var applySearch = function(){
+//    table.columns().every( function () {
+//            var that = this;
+//
+//            $( 'input', this.footer() ).on( 'keyup change', function () {
+//                if ( that.search() !== this.value ) {
+//                    that
+//                        .search( this.value )
+//                        .draw();
+//                }
+//            } );
+//        } );
+//}
+
 $( document ).ready(function() {
-    var providers = JSON.parse(data);
-    var provider = $(document.createElement('<li>'));
-    $('#providers').append(provider);
+    data.forEach(populateRow);
+    var table = $('#providers').DataTable();
 });
