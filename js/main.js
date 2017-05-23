c8c823edc5cfa5ae91f6ea38d526ee18e2ada97a
zstuder25 <zstuder25@gmail.com>
2017-05-23 00:18:39 -0400

    bootstrap added, submit button function, data still needs added to the datatable

--- js/main.js
+++ js/main.js
@@ -1,41 +1,49 @@
-var populateRow = function(item){
+var populateRow = function(item, flag = false){
     var provider = $('<tr/>')
+            .attr('id', item['first_name'] + '_' + item['last_name'])
             .prepend($('<input/>')
                 .attr('type', 'checkbox')
             )
             .append($('<td/>')
                 .attr('id', 'name')
                 .text(item['last_name'] + ', ' + item['first_name'])
+                .append($('<div/>')
+                    .attr('id', 'email')
+                    .text(item['email_address'])
+                )
             )
             .append($('<td/>')
                 .attr('id', 'specialty')
                 .text(item['specialty'])
+                .append($('<div/>')
+                    .text(item['practice_name'])
+                )
             )
-            .append($('<td/>')
-                .text(item['email_address'])
-            )
-            .append($('<td/>')
-                .text(item['practice_name'])
-            );
 
         $('#tableBody').append(provider);
 }
 
-//var applySearch = function(){
-//    table.columns().every( function () {
-//            var that = this;
-//
-//            $( 'input', this.footer() ).on( 'keyup change', function () {
-//                if ( that.search() !== this.value ) {
-//                    that
-//                        .search( this.value )
-//                        .draw();
-//                }
-//            } );
-//        } );
-//}
-
 $( document ).ready(function() {
     data.forEach(populateRow);
+
     var table = $('#providers').DataTable();
+    table.columns().every( function () {
+            var that = this;
+            $( 'input', this.footer() ).off( 'keyup change', function () {
+                    that
+                        .search( this.value )
+                        .draw();
+            } );
+        } );
+
+    $( "#addProvider" ).submit(function( event ) {
+      var provider = $('#addProvider').serializeArray()
+        .reduce(function(obj, item) {
+           obj[item.name] = item.value;
+           return obj;
+        }, {});
+        table.row.add(populateRow(provider))
+      return false;
+    });
+
 });
