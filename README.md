0567a378aadaf7b4c78405942110a9d39b9f702f
zstuder25 <zstuder25@gmail.com>
2017-05-24 23:50:44 -0400

    changed submit to prepend instead of just add, working header sorting, comments, updated readme
    changed submit to prepend instead of just add, working header sorting
    through

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # UpdoxUIChallenge
 Updox UI coding challenge
+This is a UI coding challenge. This solution utilizes jQuery and DataTables on top of the given JavaScript, HTML, and
+CSS to provide the solution. The main.html is the single web page that provides a form to create providers as well as a
+table that contains the existing providers. A user may enter a new provider and once submitted, the provider is
+prepended into the existing provider table. A user may also check any provider he or she wishes and remove it from the
+table as well. The table itself provides a search that updates on each keystroke, sorting (both asc and desc) by
+clicking on the heading of the desired column, and a scroll on the right hand side (which also eliminates the need for
+pagination).
--- js/main.js
+++ js/main.js
@@ -1,47 +1,53 @@
+//Method that creates rows to be placed in the DataTable
 function createRow(item){
+//I originally had this matching to how the mockup looked, however, I found it extremely difficult
+//to try and sort by columns that way, so I changed it more to a default Datatables look to satisfy
+//the sort functionality requirement over the aesthetics
     return $('<tr/>')
-        .attr('id', item['first_name'] + '_' + item['last_name'])
-        .prepend($('<input/>')
-            .attr('type', 'checkbox')
-            .attr('class', 'checkbox')
+        .append($('<td/>')
+            .append($('<input>')
+            .attr('type', 'checkbox'))
         )
         .append($('<td/>')
             .attr('class', 'name')
             .text(item['last_name'] + ', ' + item['first_name'])
-            .append($('<div/>')
-                .attr('class', 'email')
-                .text(item['email_address'])
-            )
         )
         .append($('<td/>')
-            .attr('class', 'specialty1')
+            .text(item['email_address'])
+        )
+        .append($('<td/>')
             .text(item['specialty'])
-            .append($('<div/>')
-                .text(item['practice_name'])
-            )
+        )
+        .append($('<td/>')
+            .text(item['practice_name'])
         );
 }
 
+//Method called from the forEach method
 var populateRow = function(item){
     var provider = createRow(item);
     $('#tableBody').append(provider);
 }
 
+//Method to remove selected entries from the DataTable
 function removeEntries(){
     var table = $('#providers').DataTable();
     var rows = table.rows().nodes();
     for(var i = 0; i < rows.length; i++){
-        if(rows[i].firstChild.checked){
+        if(rows[i].firstChild.firstChild.checked){
             table.row(rows[i]).remove().draw();
         }
     }
 }
 
 $( document ).ready(function() {
+    //Populate rows for table with the initial data
     data.forEach(populateRow);
 
+    //Initialize and create the DataTable
     var table = $('#providers').DataTable({
-          "scrollY":        "400px",
+          //Scroll functionality
+          "scrollY":        "200px",
           "scrollCollapse": true,
           "paging":         false
       });
@@ -54,13 +60,17 @@ $( document ).ready(function() {
             } );
         } );
 
+    //Setup listener for the submit button to get the form data, serialize it, and pass it into the row creator
     $( "#addProvider" ).submit(function( event ) {
       var provider = $('#addProvider').serializeArray()
         .reduce(function(obj, item) {
            obj[item.name] = item.value;
            return obj;
         }, {});
-        table.row.add(createRow(provider)).draw(false);
+        var row = createRow(provider);
+        table.row.add(row);
+        //Prepend the row instead of just adding it
+        $('#providers tbody').prepend(row);
       return false;
     });
 
